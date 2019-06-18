09962beb7cf2267571957e89db54816b9b2700d1
Soo Yeon Shim <soo@MacBook-Pro-de-Soo.local>
2019-06-18 11:55:57 -0400

    hover effects hodified

--- scripts.js
+++ scripts.js
@@ -12,15 +12,13 @@ portfolio.smoothScroll = function () {
 // eventlisteners
 portfolio.displayNav = () => { 
   $(".hamburger").on("click", function() {
-  $(".navContainer").removeClass("hide");
-  $(".hamburger").addClass("hide");
+    $(".navContainer").addClass("displayNav");
   })
 };
 
 portfolio.hideNav = () => { 
-  $(".close span").on("click", function() {
-  $(".navContainer").addClass("hide");
-  $(".hamburger").removeClass("hide");
+  $(".close").on("click", function() {
+    $(".navContainer").removeClass("displayNav");
   })
 };
 
@@ -28,6 +26,7 @@ portfolio.addScroll = () => {
   $("a").on("click", portfolio.smoothScroll());
 }
 
+// Form error handlers
 portfolio.validateEmailString = (email) => {
   const emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return emailRegex.test(String(email).toLowerCase());
@@ -38,7 +37,7 @@ portfolio.validateEmail = () => {
   const $email = $("#email").val();
 
   if (!portfolio.validateEmailString($email) || $email === "") {
-    $emailErrorMessage.removeClass("fadeIn");
+    $emailErrorMessage.removeClass("fade");
   } else {
     return true
   }
@@ -49,7 +48,7 @@ portfolio.validateName = () => {
   const $nameErrorMessage = $(".nameErrorMessage");
 
   if ($name === "") {
-    $nameErrorMessage.removeClass("fadeIn");
+    $nameErrorMessage.removeClass("fade");
   } else {
     return true
   }
@@ -60,14 +59,15 @@ portfolio.validateMessage = () => {
   const $textareaErrorMessage = $(".textareaErrorMessage");
 
   if ($textarea === "") {
-    $textareaErrorMessage.removeClass("fadeIn");
+    $textareaErrorMessage.removeClass("fade");
   } else {
     return true
   }
 }
 
+// eventlistener to trigger form validation
 portfolio.validateForm = () => {
-  $("#submit").on("click", function(e) {
+  $("#submit").on("click", (e) => {
     portfolio.validateName();
     portfolio.validateEmail();
     portfolio.validateMessage();
@@ -80,14 +80,33 @@ portfolio.validateForm = () => {
   });
 };
 
+portfolio.clearErrorMessages = () => {
+  $("#name").on("select click focus", function(){
+    $(".nameErrorMessage").addClass("fade");
+  });
+  $("#email").on("select click focus", function(){
+    $(".emailErrorMessage").addClass("fade");
+  });
+  $("#message").on("select click focus", function(){
+    $(".textareaErrorMessage").addClass("fade");
+  });
+}
 
+portfolio.clearForm = () => {
+  $("form").trigger("reset");
+}
+
+// init
 portfolio.init = () => {
   portfolio.displayNav();
   portfolio.hideNav();
   portfolio.addScroll();
   portfolio.validateForm();
+  portfolio.clearErrorMessages();
+  portfolio.clearForm();
 }
 
+// doc ready
 $(document).ready(function() {
   portfolio.init();
 })
